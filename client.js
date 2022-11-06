9a34d46dc544a2c79d78a79a2ddf70207abc9130
Manuel <manuel.nenninger@web.de>
2022-11-06 09:05:38 +0100

    leticias sanity account added

--- client.js
+++ client.js
@@ -2,7 +2,8 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: 'an6xls1b', // you can find this in sanity.json
+  projectId: '9avt4mu9', // you can find this in sanity.json
+  //projectId: 'an6xls1b', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true // `false` if you want to ensure fresh data
 })
